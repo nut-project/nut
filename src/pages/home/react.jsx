85437490c07660b21b2e3c1c0a57ae9a753a49b0
fengzilong <feng_zilong@163.com>
2019-06-13 14:36:02 +0800

    feat: support react

---
+++ src/pages/home/react.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Page extends React.Component {
+  render() {
+    return <div style={{ color: 'blue' }}>react!!!</div>
+  }
+}
+
+// export default () => <div style={{ color: 'blue' }}>react</div>
