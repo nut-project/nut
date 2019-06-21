14c8d286afe970f944a2ce0fb981c3ef6ddf7475
centmaster <861676131@qq.com>
2019-06-21 21:30:07 +0800

    add cli

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -5,29 +5,40 @@ const app = require('../lib')
 const process = require('child_process')
 const path = require('path')
 
-cli.option('--prod', 'build in production mode')
+
+  //default command
+  cli
+  // Simply omit the command name, just brackets
+  .command('[...files]', 'Build files default')
+  .option('--prod', 'build in production mode')
+  .action((files, options) => {
+    //parse file
+    parseFile(options.prod)
+  })
+
 
 cli
-  .command('create <filename>', 'Generate a new project to target folder')
+  .command('create <projectname>', 'Generate a new project to target folder')
   .option(
     '--npm-client <client>',
     `Choose an npm client for installing packages ('yarn' | 'npm' | 'pnpm')`
   )
-  .action(async (dir, {npmClient}) => {
-    //sao 脚手架
-    let command = 'sao ' + __dirname + '/../sao-sample ./' + dir;
-
+  .action(async (dir, { npmClient }) => {
     const sao = require('sao')
-
-    const app = sao({
+    const saoGenerator = sao({
       generator: path.join(__dirname, '../sao-sample'),
       outDir: dir,
       npmClient
     })
 
-    await app.run().catch(sao.handleError)
+    await saoGenerator.run().then((res) => {
+      //parse file
+      // parseFile(parsed.options.prod);
+    }).catch(saoGenerator.handleError)
 
   })
+
+
 
 cli.version(require('../package.json').version)
 
@@ -35,8 +46,12 @@ cli.help()
 
 const parsed = cli.parse()
 
-// if ( parsed.options.prod ) {
-//   app.prod()
-// } else {
-//   app.dev()
-// }
+
+
+function parseFile(isProd) {
+  if (isProd) {
+    app.prod();
+    return;
+  }
+  app.dev();
+}
