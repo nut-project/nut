e924e2c86984a02a8eea7d20e52636d2de397acd
fengzilong <feng_zilong@163.com>
2019-10-09 13:03:53 +0800

    refactor: rename file

--- packages/driver-pages/lib/index.js
+++ packages/driver-pages/lib/index.js
@@ -1,8 +1,7 @@
-const commands = require( './register-commands' )
 const pkg = require( '../package.json' )
 
 exports.name = pkg.name
 exports.version = pkg.version
 exports.apply = function ( { register } ) {
-  register( 'pages', commands )
+  register( 'pages', require( './commands' ) )
 }
