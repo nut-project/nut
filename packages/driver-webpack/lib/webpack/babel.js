b8ec45273c90dfeb1c98ba457ff2b9416f66bf41
fengzilong <feng_zilong@163.com>
2019-11-27 14:38:48 +0800

    refactor: expose cheerio api

--- packages/driver-webpack/lib/webpack/babel.js
+++ packages/driver-webpack/lib/webpack/babel.js
@@ -14,6 +14,5 @@ exports.extend = function ( config, context = {} ) {
     .loader( require.resolve( './babel/loader' ) )
     .options( {
       cacheCompression: env === 'production',
-      presets: []
     } )
 }
--- packages/driver-webpack/lib/webpack/html.js
+++ packages/driver-webpack/lib/webpack/html.js
@@ -1,5 +1,49 @@
 const path = require( 'path' )
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+const cheerio = require( 'cheerio' )
+
+class HtmlCheerioPlugin {
+  constructor( callbacks ) {
+    this.callbacks = callbacks || []
+  }
+
+  apply( compiler ) {
+    const process = async data => {
+      const callbacks = this.callbacks || []
+
+      await callbacks.reduce( async ( last, callback ) => {
+        await last
+        const $ = cheerio.load( data.html )
+        await callback( $ )
+        data.html = $.html()
+      }, Promise.resolve() )
+
+      return data
+    }
+
+    compiler.hooks.compilation.tap( 'HtmlCheerioPlugin', compilation => {
+      if ( HtmlWebpackPlugin.getHooks ) {
+        // html-webpack-plugin@4
+        HtmlWebpackPlugin
+          .getHooks( compilation )
+          .beforeEmit
+          .tapPromise(
+            'HtmlCheerioPlugin',
+            process
+          )
+      } else if ( compilation.hooks.htmlWebpackPluginAfterHtmlProcessing ) {
+        // html-webpack-plugin@3
+        compilation
+          .hooks
+          .htmlWebpackPluginAfterHtmlProcessing
+          .tapPromise(
+            'HtmlCheerioPlugin',
+            process
+          )
+      }
+    } )
+  }
+}
 
 exports.extend = function ( config, context = {} ) {
   const { userConfig = {} } = context
@@ -72,4 +116,25 @@ exports.extend = function ( config, context = {} ) {
       .plugin( 'html' )
       .use( HtmlWebpackPlugin, [ page ] )
   }
+
+  // for expose cheerio
+  config.plugin( 'html-cheerio' )
+    .use( HtmlCheerioPlugin )
+}
+
+exports.expose = function ( driver ) {
+  driver.expose( 'cheerio', callback => {
+    if ( typeof callback !== 'function' ) {
+      return
+    }
+
+    driver.useHook( 'dangerously_chainWebpack', config => {
+      config.plugin( 'html-cheerio' )
+        .tap( ( [ callbacks = [] ] = [] ) => {
+          callbacks.push( callback )
+
+          return [ callbacks ]
+        } )
+    } )
+  } )
 }
