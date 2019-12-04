2b7de9e03c9a55fd1063539dff12c2a21693552a
lx <liuxinfed@163.com>
2019-12-04 14:28:02 +0800

    feat: add config

--- examples/gene/presets-package.js
+++ examples/gene/presets-package.js
@@ -2,7 +2,8 @@ module.exports = [
   {
     cli: require.resolve( '@nut-project/cli-pages' ),
     drivers: [
-      require.resolve( '@nut-project/driver-webpack' )
+      require.resolve( '@nut-project/driver-webpack' ),
+      require.resolve( '@nut-project/driver-mock' )
     ],
     plugins: {
       clearConsole: '@nut-project/driver-webpack/plugins/clear-console',
