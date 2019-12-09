7daeb91f44929289a0e34339941c28adbae10ec9
fengzilong <feng_zilong@163.com>
2019-12-09 21:16:47 +0800

    refactor: add fallback output.path

--- packages/driver-webpack/lib/webpack/outdir.js
+++ packages/driver-webpack/lib/webpack/outdir.js
@@ -4,5 +4,7 @@ exports.extend = function ( config, context = {} ) {
   const outdir = userConfig.output && userConfig.output.path
   if ( outdir ) {
     config.output.path( outdir )
+  } else {
+    config.output.path( 'dist' )
   }
 }
